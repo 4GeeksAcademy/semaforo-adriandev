9757700ba58d64551ee69a248ab15d907400f989
adriandev17 <116568010+adriandev17@users.noreply.github.com>
2025-11-29 03:33:42 +0000

    Primer desafio

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react"; 
+
+const TrafficLight = () => {
+
+	const [ color, setColor ] = useState("red");
+
+	return <div className="container-fluid mt-4 p-4">
+		<div className="container-fluid bg-dark" style={{ width: "10px", height: "60px" }}> </div>
+		<div className="container-fluid bg-dark p-4" style={{ width: "80px", height: "200px" }}>
+			<div class="btn-group-vertical d-flex align-items" role="group" aria-label="Basic mixed styles example">
+				<button onClick={ () => setColor("red") } type="button" class={"btn btn-danger mb-3 rounded-circle " + (color == "red" ? "on" : "" )} style={{ width: "40px", height: "40px" }}></button>
+				<button onClick={ () => setColor("yellow") } type="button" class={"btn btn-warning mb-3 rounded-circle " + (color == "yellow" ? "on" : "" )}  style={{ width: "40px", height: "40px" }}></button>
+				<button onClick={ () => etColor("green") } type="button" class={"btn btn-success mb-3 rounded-circle " + (color == "green" ? "on" : "" )}  style={{ width: "40px", height: "40px" }}></button>
+			</div>
+		</div>
+	</div>
+};
+
+export default TrafficLight;
